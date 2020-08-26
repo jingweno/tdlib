6de0d83e07b9ab5345ec01531fcf8228aa32141a
Owen Ou <jingweno@gmail.com>
2020-08-26 15:46:01 -0700

    Convert int64 type to Dart string
    For some reason, int64 is returned as string from the Telegram server.
    Adjust the generator to generate string type for int64 fields.
    
    This fixes https://github.com/i-Naji/tdlib/issues/15.

--- lib/src/tdapi/convertor.dart
+++ lib/src/tdapi/convertor.dart
@@ -3,6 +3,8 @@ part of 'tdapi.dart';
 TdObject convertToObject(String query) {
   if (query != null) {
     final newJson = json.decode(query);
+    print("convertToObject");
+    print(newJson);
     final object = allObjects[newJson['@type']](newJson);
     return object;
   } else {
